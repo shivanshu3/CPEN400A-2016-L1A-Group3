4d24a666e4b2718dde9df47f23aa80554ea90bcb
Armin Rezaiean-Asel <armin.rezaiean.asel@gmail.com>
2016-10-14 22:24:59 -0700

    adding folder for assignment-3

---
+++ assignment-3/js/cart.js
@@ -0,0 +1,274 @@
+/**
+ * Sets up and initializes the global variables used in this app.
+ */
+function initGlobalVars() {
+	window.cart = {};
+	// These are the paths of product images
+	window.productsPrices = [
+		"images/Box1_$10.png",
+		"images/Box2_$5.png",
+		"images/Clothes1_$20.png",
+		"images/Clothes2_$30.png",
+		"images/Jeans_$50.png",
+		"images/Keyboard_$20.png",
+		"images/KeyboardCombo_$40.png",
+		"images/Mice_$20.png",
+		"images/PC1_$350.png",
+		"images/PC2_$400.png",
+		"images/PC3_$300.png",
+		"images/Tent_$100.png"
+	];
+	window.products = initProductsVar(productsPrices);
+	window.inactiveTime = 0;
+	window.cartDisplayRunning = false;
+}
+
+/**
+ * Initializes the products global hashmap which is a map of <ProductName> to
+ * <ProductPrice>
+ * It requires an array of image paths as input. Each element of the array
+ * should be an image path with the image name of the following format:
+ * <product_name>_$<product_price>.<extension>
+ * Returns a hashmap, which can be assigned to the global variable 'products'
+ */
+function initProductsVar(productsPrices) {
+	var products = {};
+
+	for (var i = 0; i < productsPrices.length; i++) {
+		var imagePath = productsPrices[i];
+		var productName = productNameFromImagePath(imagePath);
+
+		// All quatities are initialized to 5:
+		products[productName] = 5;
+	}
+
+	return products;
+}
+
+/**
+ * Given an image path like "images/Box1_$10.png", it returns the product
+ * name.
+ */
+function productNameFromImagePath(imagePath) {
+	// This is the full filename, ex:
+	// "Box1_$10.png"
+	var imageFileName = imagePath.match(/[\w\$]+\.\w+/)[0];
+
+	// This is the filename without the extension, ex:
+	// "Box1_$10"
+	imageFileName = imageFileName.split('.')[0];
+
+	// This is the product name, ex:
+	// "Box1"
+	var productName = imageFileName.split('_')[0];
+
+	return productName;
+}
+
+/**
+ * Given an image path like "images/Box1_$10.png", it returns the product
+ * price.
+ */
+function productPriceFromImagePath(imagePath) {
+	// This is the full filename, ex:
+	// "Box1_$10.png"
+	var imageFileName = imagePath.match(/[\w\$]+\.\w+/)[0];
+
+	// This is the filename without the extension, ex:
+	// "Box1_$10"
+	imageFileName = imageFileName.split('.')[0];
+
+	// This is the price of the product, ex: 10
+	var productPrice = Number(imageFileName.split('_')[1].substring(1));
+
+	return productPrice;
+}
+
+/**
+ * Enhances the DOM and initializes the inactive timer.
+ */
+function initPage() {
+	console.log('Initializing page...');
+
+	// Initialization functions:
+	setInterval(inactiveTimeTracking, 1000);
+	initProducts();
+}
+
+/**
+ * Initializes all the .product divs.
+ * Adds the cart image, price, title, and add and remove buttons to
+ * all product divs.
+ */
+function initProducts() {
+	var productListDiv = $('#productList');
+
+	for (var i = 0; i < productsPrices.length; i++) {
+		// This is a div with the class 'product':
+		var productDiv = $('<div>');
+		productDiv.addClass('product');
+
+		// This is the path of the image, ex:
+		// "images/Box1_$10.png"
+		var imagePath = productsPrices[i];
+
+		// This is the product name, ex:
+		// "Box1"
+		var productName = productNameFromImagePath(imagePath);
+
+		// This is the price of the product with the dollar sign, ex:
+		// "$10"
+		var productPrice = '$' + productPriceFromImagePath(imagePath);
+
+		// Preparing divs to be inserted into the product div:
+
+		var productImage = $('<img>');
+		productImage.attr('src', imagePath);
+
+		var cartDiv = $('<div>');
+		cartDiv.addClass('cart');
+		var cartImage = $('<img>');
+		cartImage.addClass('cartimg');
+		cartImage.attr('src', 'images/cart.png');
+		cartDiv.append(cartImage);
+
+		var priceDiv = $('<div>');
+		priceDiv.addClass('price')
+		priceDiv.text(productPrice);
+
+		// Anonymous function is used here to store productName in its closure:
+		var addButton;
+		(function() {
+			var _productName = productName;
+			addButton = $('<button>');
+			addButton.addClass('add');
+			addButton.text('Add');
+			addButton.click(function() {
+				addToCart(_productName);
+			});
+		})();
+
+		// Anonymous function is used here to store productName in its closure:
+		var removeButton;
+		(function() {
+			var _productName = productName;
+			removeButton = $('<button>');
+			removeButton.addClass('remove');
+			removeButton.text('Remove');
+			removeButton.click(function() {
+				removeFromCart(_productName);
+			});
+		})();
+
+		var titleHeading = $('<h5>');
+		titleHeading.text(productName);
+
+		// Insert all the divs and buttons created above to the product div:
+		productDiv.append(productImage);
+		productDiv.append(cartDiv);
+		productDiv.append(priceDiv);
+		productDiv.append(addButton);
+		productDiv.append(removeButton);
+		productDiv.append(titleHeading);
+
+		// Insert this product div into the productList div:
+		productListDiv.append(productDiv);
+	}
+}
+
+/**
+ * This function is called every second. It is used to keep track of how
+ * long the user was inactive for.
+ */
+function inactiveTimeTracking() {
+	if(cartDisplayRunning)
+		return;
+
+	inactiveTime += 1;
+
+	if(inactiveTime > 30) {
+		alert("Hey there! Are you still planning to buy something?");
+		inactiveTime = 0;
+	}
+
+}
+
+/**
+ * Adds the given product to the cart.
+ * If we run out of this product, then that product is not added to the cart.
+ */
+function addToCart(productName) {
+	inactiveTime = 0;
+
+	var itemQuantity = products[productName];
+
+	if(itemQuantity === 0) {
+		alert(productName + " is no longer in stock.");
+		return;
+	}
+
+	products[productName] = itemQuantity - 1;
+
+	var inCartQuantity = cart[productName];
+
+	if(inCartQuantity === undefined)
+		cart[productName] = 1;
+	else
+		cart[productName] = inCartQuantity + 1;
+
+	alert(productName + " was added to your cart.");
+}
+
+/**
+ * Removes the given product from the cart if the user has
+ * the item in his/her cart.
+ */
+function removeFromCart(productName) {
+	inactiveTime = 0;
+
+	var inCartQuantity = cart[productName];
+
+	if(inCartQuantity === undefined)
+		alert(productName + " is not in your cart.");
+	else {
+		if(inCartQuantity === 1)
+			delete cart[productName];
+		else
+			cart[productName] = inCartQuantity - 1;
+
+		products[productName] += 1;
+		alert(productName + " was removed from your cart.");
+	}
+}
+
+/**
+ * Shows the cart info the user by using alerts.
+ */
+function showCart() {
+	cartDisplayRunning = true;
+	var productsToDisplay = [];
+	for(var item in cart) {
+		productsToDisplay.push(item + ': ' + cart[item]);
+	}
+
+	showItemsWithInterval(0, productsToDisplay);
+}
+
+/**
+ * Works in conjunction with the showCart function.
+ */
+function showItemsWithInterval(index, productsToDisplay) {
+	if(productsToDisplay.length > index) {
+		alert(productsToDisplay[index]);
+		index += 1;
+
+		if(productsToDisplay.length > index) {
+			setTimeout(showItemsWithInterval, 5000, index, productsToDisplay);
+			return;
+		}
+	}
+
+	alert("Your entire shopping cart has been displayed.");
+	cartDisplayRunning = false;
+	inactiveTime = 0;
+}
