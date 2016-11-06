3719c5dd87289394bddb9d8ede1beb17f7d99950
Armin Rezaiean-Asel <armin.rezaiean.asel@gmail.com>
2016-11-06 01:18:10 -0800

    copying over assignment3 folder for assignment4

---
+++ assignment-4/js/cart.js
@@ -0,0 +1,304 @@
+/**
+ * Sets up and initializes the global variables used in this app.
+ */
+function initGlobalVars() {
+	window.cart = {};
+	// These are the paths of product images
+	window.productsPrices = [
+		"images/Box1_$10.png",
+		"images/Box2_$5.png",
+		"images/Clothes1_$20.png",
+		"images/Clothes2_$30.png",
+		"images/Jeans_$50.png",
+		"images/Keyboard_$20.png",
+		"images/KeyboardCombo_$40.png",
+		"images/Mice_$20.png",
+		"images/PC1_$350.png",
+		"images/PC2_$400.png",
+		"images/PC3_$300.png",
+		"images/Tent_$100.png"
+	];
+	window.products = initProductsVar(productsPrices);
+	window.inactiveTime = 0;
+	window.cartTotal = 0;
+	window.cartModal = null;
+}
+
+/**
+ * Initializes the products global hashmap which is a map of <ProductName> to
+ * <ProductPrice>
+ * It requires an array of image paths as input. Each element of the array
+ * should be an image path with the image name of the following format:
+ * <product_name>_$<product_price>.<extension>
+ * Returns a hashmap, which can be assigned to the global variable 'products'
+ */
+function initProductsVar(productsPrices) {
+	var products = {};
+
+	for (var i = 0; i < productsPrices.length; i++) {
+		var imagePath = productsPrices[i];
+		var productName = productNameFromImagePath(imagePath);
+		var productPrice = productPriceFromImagePath(imagePath);
+
+		// All quatities are initialized to 5, and all prices are initialized
+		// to the product's respective price amount
+		products[productName] = {
+			'price' : productPrice,
+			'quantity' : 5,
+		};
+	}
+
+	return products;
+}
+
+/**
+ * Given an image path like "images/Box1_$10.png", it returns the product
+ * name.
+ */
+function productNameFromImagePath(imagePath) {
+	// This is the full filename, ex:
+	// "Box1_$10.png"
+	var imageFileName = imagePath.match(/[\w\$]+\.\w+/)[0];
+
+	// This is the filename without the extension, ex:
+	// "Box1_$10"
+	imageFileName = imageFileName.split('.')[0];
+
+	// This is the product name, ex:
+	// "Box1"
+	var productName = imageFileName.split('_')[0];
+
+	return productName;
+}
+
+/**
+ * Given an image path like "images/Box1_$10.png", it returns the product
+ * price.
+ */
+function productPriceFromImagePath(imagePath) {
+	// This is the full filename, ex:
+	// "Box1_$10.png"
+	var imageFileName = imagePath.match(/[\w\$]+\.\w+/)[0];
+
+	// This is the filename without the extension, ex:
+	// "Box1_$10"
+	imageFileName = imageFileName.split('.')[0];
+
+	// This is the price of the product, ex: 10
+	var productPrice = Number(imageFileName.split('_')[1].substring(1));
+
+	return productPrice;
+}
+
+/**
+ * Enhances the DOM and initializes the inactive timer.
+ */
+function initPage() {
+	console.log('Initializing page...');
+
+	// Initialization functions:
+	setInterval(inactiveTimeTracking, 1000);
+	initProducts();
+
+	// Cart button click handler:
+	window.cartModal = new Modal(700);
+	$('#cartButton').click(function() {
+		window.cartModal.show();
+		inactiveTime = 0;
+		displayInactiveTime(inactiveTime);
+	});
+}
+
+/**
+ * Initializes all the .product divs.
+ * Adds the cart image, price, title, and add and remove buttons to
+ * all product divs.
+ */
+function initProducts() {
+	var productListDiv = $('#productList');
+
+	for (var i = 0; i < productsPrices.length; i++) {
+		// This is a div with the class 'product':
+		var productDiv = $('<div>');
+		productDiv.addClass('product');
+
+		// This is the path of the image, ex:
+		// "images/Box1_$10.png"
+		var imagePath = productsPrices[i];
+
+		// This is the product name, ex:
+		// "Box1"
+		var productName = productNameFromImagePath(imagePath);
+
+		// This is the price of the product with the dollar sign, ex:
+		// "$10"
+		var productPrice = '$' + productPriceFromImagePath(imagePath);
+
+		// Add IDs to the product divs for easy retrieval later on:
+		productDiv.attr('id', 'product_' + productName);
+
+		// Preparing divs to be inserted into the product div:
+
+		var productImage = $('<img>');
+		productImage.attr('src', imagePath);
+
+		var cartDiv = $('<div>');
+		cartDiv.addClass('cart');
+		var cartImage = $('<img>');
+		cartImage.addClass('cartimg');
+		cartImage.attr('src', 'images/cart.png');
+		cartDiv.append(cartImage);
+
+		var priceDiv = $('<div>');
+		priceDiv.addClass('price')
+		priceDiv.text(productPrice);
+
+		// Anonymous function is used here to store productName in its closure:
+		var addButton;
+		(function() {
+			var _productName = productName;
+			addButton = $('<button>');
+			addButton.addClass('add');
+			addButton.text('Add');
+			addButton.click(function() {
+				addToCart(_productName);
+			});
+		})();
+
+		// Anonymous function is used here to store productName in its closure:
+		var removeButton;
+		(function() {
+			var _productName = productName;
+			removeButton = $('<button>');
+			removeButton.addClass('remove');
+			removeButton.text('Remove');
+			removeButton.click(function() {
+				removeFromCart(_productName);
+			});
+		})();
+
+		var titleHeading = $('<h5>');
+		titleHeading.text(productName);
+
+		// Insert all the divs and buttons created above to the product div:
+		productDiv.append(productImage);
+		productDiv.append(cartDiv);
+		productDiv.append(priceDiv);
+		productDiv.append(addButton);
+		productDiv.append(removeButton);
+		productDiv.append(titleHeading);
+
+		// Insert this product div into the productList div:
+		productListDiv.append(productDiv);
+	}
+}
+
+/**
+ * This function is called every second. It is used to keep track of how
+ * long the user was inactive for.
+ */
+function inactiveTimeTracking() {
+	if(window.cartModal.isVisible()) {
+		return;
+	}
+
+	inactiveTime += 1;
+	displayInactiveTime(inactiveTime);
+
+	if(inactiveTime > 300) {
+		alert("Hey there! Are you still planning to buy something?");
+		inactiveTime = 0;
+	}
+
+}
+
+/**
+ * Updates the inactive timer in the footer.
+ */
+function displayInactiveTime(time) {
+	var inactiveTimeSpan = $('#inactive_time_span');
+	inactiveTimeSpan.text(time);
+}
+
+/**
+ * Adds the given product to the cart.
+ * If we run out of this product, then that product is not added to the cart.
+ * Returns true if the op is successful, false otherwise.
+ */
+function addToCart(productName) {
+	inactiveTime = 0;
+
+	var itemQuantity = products[productName].quantity;
+
+	if(itemQuantity === 0) {
+		alert(productName + " is no longer in stock.");
+		return false;
+	}
+
+	products[productName].quantity = itemQuantity - 1;
+
+	var inCartQuantity = cart[productName];
+
+	if(inCartQuantity === undefined) {
+		cart[productName] = 1;
+		showRemoveButton(true, productName);
+	} else {
+		cart[productName] = inCartQuantity + 1;
+	}
+
+	cartTotal += products[productName].price;
+	updateCartTotal(cartTotal);
+	alert(productName + " was added to your cart.");
+	return true;
+}
+
+/**
+ * Removes the given product from the cart if the user has
+ * the item in his/her cart.
+ * Returns true if the op is successful, false otherwise.
+ */
+function removeFromCart(productName) {
+	inactiveTime = 0;
+
+	var inCartQuantity = cart[productName];
+
+	if(inCartQuantity === undefined) {
+		alert(productName + " is not in your cart.");
+		return false;
+	} else {
+		if(inCartQuantity === 1) {
+			delete cart[productName];
+			showRemoveButton(false, productName);
+		} else {
+			cart[productName] = inCartQuantity - 1;
+		}
+
+		products[productName].quantity += 1;
+
+		cartTotal -= products[productName].price;
+		updateCartTotal(cartTotal);
+		alert(productName + " was removed from your cart.");
+		return true;
+	}
+}
+
+/**
+ * Updates the total amount in the cart button.
+ */
+function updateCartTotal(cartTotal) {
+	var cartButton = $('#cartButton');
+	cartButton.text('Cart($' + cartTotal + ')');
+}
+
+/**
+ * This function enables remove button visibility when hover over a given product, if
+ * the product is currently in the shopper's cart.
+ */
+function showRemoveButton(show, productName) {
+	var removeButton = $('#product_' + productName + ' button.remove');
+	if (show) {
+		removeButton.css('display', 'block');
+	} else {
+		removeButton.css('display', 'none');
+	}
+}
---
+++ assignment-4/js/modal.js
@@ -0,0 +1,267 @@
+/**
+ * The Modal class represents a Modal window.
+ * Currently, this only supports 1 model window at a time.
+ */
+
+/** PUBLIC METHODS **/
+
+/**
+ * The constructor.
+ * It creates the modal window in the DOM, but is not active at this time.
+ * The width of the modal is set to what's passed to the constructor.
+ */
+var Modal = function(width) {
+	// Instance variables:
+	this.width = width;
+	this.itemsTable = null;
+	this.footerDiv = null;
+
+	// Create the main DOM elements:
+	this.backgroundDiv = this.createBackgroundDiv();
+	this.windowDiv = this.createWindowDiv();
+	this.closeButton = this.createCloseButton();
+	this.contentsDiv = this.createContentsDiv();
+
+	// Hide them before we add them to the DOM:
+	this.hide();
+
+	// Add everything to the DOM:
+	$('body').append(this.backgroundDiv);
+	$('body').append(this.windowDiv);
+	this.windowDiv.append(this.closeButton);
+	this.windowDiv.append(this.contentsDiv);
+};
+
+/**
+ * Shows the modal window.
+ */
+Modal.prototype.show = function() {
+	// This will update the cart contents view:
+	this.refreshView();
+
+	this.backgroundDiv.css('display', 'block');
+	this.windowDiv.css('display', 'block');
+	var _this = this;
+
+	// Esc key handler:
+	$(document).on('keyup.modal', function(e) {
+		if (e.keyCode == 27) {
+			_this.hide();
+		}
+	});
+};
+
+/**
+ * Hides the modal window.
+ */
+Modal.prototype.hide = function() {
+	this.backgroundDiv.css('display', 'none');
+	this.windowDiv.css('display', 'none');
+
+	// Unbind the esc key handler:
+	$(document).unbind('keyup.modal');
+};
+
+/**
+ * Returns true if the modal window is visible, false otherwise.
+ */
+Modal.prototype.isVisible = function() {
+	return this.backgroundDiv.css('display') == 'block';
+};
+
+/** PRIVATE METHODS **/
+
+/**
+ * Creates a grey translucent div which covers the entire screen
+ * to block user input to the underlying elements.
+ */
+Modal.prototype.createBackgroundDiv = function() {
+	var backgroundDiv = $('<div>');
+	backgroundDiv.css('background-color', 'grey');
+	backgroundDiv.css('opacity', 0.5);
+	backgroundDiv.css('position', 'fixed');
+	backgroundDiv.css('z-index', 1000);
+	backgroundDiv.css('top', 0);
+	backgroundDiv.css('bottom', 0);
+	backgroundDiv.css('left', 0);
+	backgroundDiv.css('right', 0);
+
+	return backgroundDiv;
+};
+
+/**
+ * Creates a window div in the center of the screen on top of the
+ * background div. This is the main div inside of which everything else goes.
+ */
+Modal.prototype.createWindowDiv = function() {
+	var windowDiv = $('<div>');
+	windowDiv.addClass('modal_window');
+	windowDiv.width(this.width);
+	windowDiv.css('position', 'fixed');
+	windowDiv.css('top', '50%');
+	windowDiv.css('left', '50%');
+	windowDiv.css('transform', 'translate(-50%, -50%)');
+	windowDiv.css('background-color', 'white');
+	windowDiv.css('z-index', 1001);
+	windowDiv.css('box-shadow', '6px 7px 30px 5px rgba(0,0,0,0.7)');
+	windowDiv.css('border-radius', 10);
+	windowDiv.css('box-sizing', 'border-box');
+	windowDiv.css('padding', 20);
+
+	return windowDiv;
+};
+
+/**
+ * Creates a close button which can be placed inside the window div.
+ */
+Modal.prototype.createCloseButton = function() {
+	var closeButton = $('<button>');
+	closeButton.css('position', 'fixed');
+	closeButton.css('right', -15);
+	closeButton.css('top', -15);
+	closeButton.css('height', 30);
+	closeButton.css('width', 30);
+	closeButton.css('background-color', 'black');
+	closeButton.css('border-style', 'solid');
+	closeButton.css('border-color', 'white');
+	closeButton.css('border-radius', 15);
+	closeButton.css('box-shadow', '0 4px 8px 0 rgba(0,0,0,0.2)');
+	closeButton.css('color', 'white');
+	closeButton.css('font-weight', 'bold');
+	closeButton.css('outline', 'none');
+
+	closeButton.text('X');
+
+	var _this = this;
+	closeButton.click(function() {
+		_this.hide();
+	});
+
+	return closeButton;
+};
+
+/**
+ * Creates a content div inside of which there is a heading, and table
+ * for showing the items in the cart, and a footer with a checkout button
+ * and a subtotal amount div.
+ */
+Modal.prototype.createContentsDiv = function() {
+	var contentsDiv = $('<div>');
+
+	var heading = $('<h1>');
+	heading.text('Your Shopping Cart');
+	heading.css('font-size', 24);
+
+	this.itemsTable = $('<table>');
+	this.itemsTable.width('100%');
+	var tableHead = $('<thead>');
+	tableHead.append('<tr><th>Item</th><th>Qty</th><th>Unit Cost</th>' +
+		'<th>Total Cost</th><th></th><th></th></tr>');
+	this.itemsTable.append(tableHead);
+	this.itemsTable.append($('<tbody>'));
+
+	this.footerDiv = $('<div>');
+	this.footerDiv.css('margin-top', 20);
+	var checkoutButton = $('<button>Checkout</button>');
+	checkoutButton.height(30);
+	checkoutButton.css('background-color', 'antiquewhite');
+	checkoutButton.css('border-style', 'solid');
+	checkoutButton.css('outline', 'none');
+	var subtotalDiv = $('<div><b>Subtotal:</b> $<span class="subtotal_span">-</span></div>');
+	subtotalDiv.css('float', 'right');
+	this.footerDiv.append(checkoutButton);
+	this.footerDiv.append(subtotalDiv);
+
+	contentsDiv.append(heading);
+	contentsDiv.append(this.itemsTable);
+	contentsDiv.append(this.footerDiv);
+
+	return contentsDiv;
+};
+
+/**
+ * Refreshes the DOM elements inside the content window.
+ * Ex: The items table and the subtotal amount div.
+ */
+Modal.prototype.refreshView = function() {
+	var _this = this;
+
+	var itemsTableBody = this.itemsTable.find('tbody');
+	itemsTableBody.empty();
+
+	var createCellButton = function(text, bgColor, textColor) {
+		var button = $('<td>');
+		button.css('padding-left', 0);
+		button.css('text-align', 'center');
+		button.css('cursor', 'pointer');
+		button.css('background-color', bgColor);
+		button.css('color', textColor);
+		button.css('user-select', 'none');
+		button.css('font-weight', '900');
+		button.width(30);
+		button.text(text);
+		return button;
+	};
+
+	for (var item in cart) {
+		var row = $('<tr>');
+		var totalCost = cart[item] * products[item].price;
+		row.append('<td class="item_name">' + item + '</td>');
+		row.append('<td class="item_quantity">' + cart[item] + '</td>');
+		row.append('<td class="item_unit_cost">$' + products[item].price + '</td>');
+		row.append('<td class="item_total_cost">$' + totalCost + '</td>');
+		var plusButton = createCellButton('+', '#a6e4a6', 'green');
+		(function() {
+			var _row = row;
+			plusButton.click(function() {
+				_this.plusButtonClicked(_row);
+			});
+		})();
+		row.append(plusButton);
+		var minusButton = createCellButton(unescape('%u2013'), '#ff8e8e', 'red');
+		(function() {
+			var _row = row;
+			minusButton.click(function() {
+				_this.minusButtonClicked(_row);
+			});
+		})();
+		row.append(minusButton);
+
+		itemsTableBody.append(row);
+	}
+
+	this.updateSubtotal();
+};
+
+/**
+ * This method is fired when the plus button inside the items table is clicked.
+ */
+Modal.prototype.plusButtonClicked = function(row) {
+	var productName = row.find('.item_name').text();
+	if (addToCart(productName)) {
+		this.refreshView();
+	}
+};
+
+/**
+ * This method is fired when the minus button inside the items table is clicked.
+ */
+Modal.prototype.minusButtonClicked = function(row) {
+	var productName = row.find('.item_name').text();
+	if (removeFromCart(productName)) {
+		this.refreshView();
+	}
+};
+
+/**
+ * Calculates and updates the subtotal amount value in the content window.
+ */
+Modal.prototype.updateSubtotal = function() {
+	var subtotal = 0;
+	for (var item in cart) {
+		subtotal += (cart[item] * products[item].price);
+	}
+
+	var subtotalSpan = this.footerDiv.find('.subtotal_span');
+	subtotalSpan.text(subtotal);
+};
