cd8875359573745e738b71940934fd1ea4548514
Shivanshu Goyal <shivanshu3@gmail.com>
2016-11-28 15:46:02 -0800

    Started writing frontend tests

---
+++ assignment-5/tests/test.js
@@ -0,0 +1,24 @@
+// It just sets up the test environment. It doesn't really test anything.
+describe('testSetup', function() {
+   it('should setup the test environment', function(done) {
+      done();
+   });
+});
+
+describe('addToCart', function() {
+   it('Should add Mice to cart', function(done) {
+      done();
+   });
+   it('Should add KeyboardCombo to cart', function(done) {
+      done();
+   });
+});
+
+describe('removeFromCart', function() {
+   it('Should remove Mice from cart', function(done) {
+      done();
+   });
+   it('Should remove KeyboardCombo from cart', function(done) {
+      done();
+   });
+});
