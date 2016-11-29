cccb95272fdf683837794cc513778a1aef0a61fe
Shivanshu Goyal <shivanshu3@gmail.com>
2016-11-28 16:21:47 -0800

    Added another addToCart test

--- assignment-5/tests/test.js
+++ assignment-5/tests/test.js
@@ -19,6 +19,25 @@ describe('addToCart', function() {
 		expect(cart.Tent).to.be(1);
 		done();
 	});
+	it('Should not add more items to the cart than in stock', function(done) {
+		var stock = products.Mice.quantity;
+		expect(stock).to.be.greaterThan(0);
+		expect(cart.Mice).to.be(undefined);
+		// Add all Mice which are in stock:
+		for(var i = 0; i < stock; i++) {
+			addToCart('Mice');
+			expect(products.Mice.quantity).to.be(stock - 1 - i);
+			expect(cart.Mice).to.be(i + 1);
+		}
+		// There should be no more Mice available in stock:
+		expect(products.Mice.quantity).to.be(0);
+		expect(cart.Mice).to.be(stock);
+		// This operation shouldn't add any more Mice to the cart:
+		addToCart('Mice');
+		expect(products.Mice.quantity).to.be(0);
+		expect(cart.Mice).to.be(stock);
+		done();
+	});
 });
 
 describe('removeFromCart', function() {
